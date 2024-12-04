88cfe63219895975515759c6de172414da22b7c0
Muhammad robah <86682953+Daehoo0@users.noreply.github.com>
2024-12-04 14:08:56 +0700

    login & kirim pesan
    tampilan login dan kirim pesan wa ke ortu

--- frontend/app.py
+++ frontend/app.py
@@ -1,14 +1,17 @@
 import time
 import pyqrcode
 import tkinter as tk
+from tkinter import font as tkFont
+from tkinter import font
 from tkinter import messagebox ,filedialog
 from PIL import Image, ImageTk
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import io
 import re
 import pandas
-
+import requests
 
 # Inisialisasi driver di luar fungsi agar dapat diakses secara global
 driver = None
@@ -31,7 +34,7 @@ def fetch_and_show_qr_code():
         driver.get("https://web.whatsapp.com/")
         
         # Tunggu sebentar agar halaman selesai dimuat
-        time.sleep(5)
+        time.sleep(4)
         
         # Tunggu elemen dengan atribut data-ref muncul
         qr_element = driver.find_element("css selector", "div[data-ref]")
@@ -64,13 +67,55 @@ def fetch_and_show_qr_code():
         messagebox.showerror("Error", f"Gagal mengambil QR Code: {str(e)}")
 
 # Fungsi ketika tombol login ditekan
+# def login():
+#     userid = username_entry.get()
+#     password = password_entry.get()
+
+#     # Prepare data to send to the backend
+#     data = {
+#         "username": userid,
+#         "password": password
+#     }
+
+#     try:
+#         # Send POST request to the Node.js login endpoint
+#         response = requests.post("http://localhost:3000/login", json=data)
+
+#         # Check the response from the server
+#         if response.status_code == 200:
+#             result = response.json()
+#             if result.get("success") == 1:
+#                 token = result.get("data")  # Retrieve the JWT token
+#                 messagebox.showinfo("Login Success", "Login successful!")
+                
+#                 # Clear the login form
+#                 username_entry.delete(0, tk.END)
+#                 password_entry.delete(0, tk.END)
+                
+#                 # Switch to the next frame or functionality
+#                 login_frame.pack_forget()
+#                 menu_frame.pack(fill="both", expand=True)
+
+#                 # Print the token for debugging purposes (you can store it securely if needed)
+#                 print("Token:", token)
+
+#             else:
+#                 messagebox.showerror("Login Failed", "Invalid credentials")
+#         else:
+#             # If the server returned an error response
+#             error_message = response.json().get("errors", [{"msg": "Unknown error"}])[0]["msg"]
+#             messagebox.showerror("Login Failed", error_message)
+
+#     except requests.exceptions.RequestException as e:
+#         # Handle connection errors
+#         messagebox.showerror("Error", f"Failed to connect to the backend: {e}")
 def login():
-    userid = username_entry.get()
+    userid = email_entry.get()
     password = password_entry.get()
 
     if userid == "admin" and password == "admin":
         # Pindah ke frame QR Code
-        username_entry.delete(0, tk.END)
+        email_entry.delete(0, tk.END)
         password_entry.delete(0, tk.END)
         login_frame.pack_forget()
         menu_frame.pack(fill="both", expand=True)
@@ -125,16 +170,46 @@ def is_logged_in():
     except Exception as e:
         return False
 
+# Array berisi data siswa
+data_siswa = [
+    {"nisn": "1234567890", "nama": "Ahmad Fauzi", "no_hp_ortu": "082140950288"},
+    {"nisn": "9876543210", "nama": "Siti Nurhaliza", "no_hp_ortu": "082140950288"},
+    {"nisn": "1122334455", "nama": "Budi Santoso", "no_hp_ortu": "082140950288"},
+    {"nisn": "5566778899", "nama": "Rina Agustina", "no_hp_ortu": "082140950288"},
+    {"nisn": "6677889900", "nama": "Yusuf Kurniawan", "no_hp_ortu": "082140950288"},
+]
+
+# Cek qrcode absen
 def on_key_release(event):
-    # Dapatkan teks dari entry
     current_text = nik_entry.get()
 
-    # Jika panjang teks sudah mencapai 9 karakter
-    if len(current_text) == 9:
-        # Tampilkan messagebox dengan nomor yang di-input
-        messagebox.showinfo("Nomor Diterima", f"Nomor yang Anda input: {current_text}")
+    if len(current_text) == 10:  # Cek jika panjang input 10 karakter
+        siswa_ditemukan = next((siswa for siswa in data_siswa if siswa["nisn"] == current_text), None)
 
-        # Kosongkan entry setelah menampilkan messagebox
+        if siswa_ditemukan:
+            nama = siswa_ditemukan["nama"]
+            no_hp = siswa_ditemukan["no_hp_ortu"]
+
+            try:
+                driver.get(f"https://web.whatsapp.com/send?phone=62{no_hp}&text=Selamat pagi Bapak/Ibu, kami ingin menyampaikan bahwa siswa {nama} telah hadir di sekolah.")
+
+                # Tunggu beberapa saat agar pengguna dapat login
+                time.sleep(15)
+
+                # Klik tombol kirim pada WhatsApp Web (dengan Xpath)
+                try:
+                    send_button = driver.find_element("css selector", 'button[aria-label="Kirim"]')
+                    send_button.click()
+                    nama = ""
+                    no_hp = ""
+                except Exception as e:
+                    messagebox.showerror("Error", f"Gagal menekan tombol kirim: {str(e)}")
+                
+            except Exception as e:
+                messagebox.showerror("Error", f"Gagal membuka WhatsApp Web: {str(e)}")
+        else:
+            messagebox.showwarning("NISN Tidak Ditemukan", "NISN tidak ditemukan!")
+        
         nik_entry.delete(0, tk.END)
 
 def sanitize_phone_number(phone):
@@ -179,12 +254,6 @@ def tomenudatabase():
     tambahsiswa_frame.pack_forget()
     menu_frame.pack(fill="both", expand=True)
 
-# def on_hover(event, canvas, color):
-#     canvas.configure(bg=color)
-
-# def on_leave(event, canvas, color):
-#     canvas.configure(bg=color)
-
 # Fungsi untuk membuat tombol melingkar
 def create_rounded_button(canvas, x, y, width, height, radius, text, command=None):
     # Gambar bentuk bulat di sekitar tombol
@@ -219,28 +288,73 @@ root.title("WhatsApp Web QR Code Fetcher")
 root.geometry("1000x1000")
 
 # Frame Login
-login_frame = tk.Frame(root)
+login_frame = tk.Frame(root, bg="white")
 login_frame.pack(fill="both", expand=True)
-# login_frame = tk.Frame(root)
 
-# Label di jendela utama
-label = tk.Label(login_frame, text="Klik tombol login untuk mengambil QR code WhatsApp Web")
-label.pack(pady=20)
+# Kiri frame
+left_frame = tk.Frame(login_frame, bg="white", width=500)
+left_frame.pack(fill="both", side="left", padx=10, pady=10)
 
-# Username dan Password
-username_label = tk.Label(login_frame, text="Username:")
-username_label.pack()
-username_entry = tk.Entry(login_frame)
-username_entry.pack()
+# Tambahkan gambar ke kiri frame
+try:
+    original_image = Image.open("assets/login_image.png")
+    resized_image = original_image.resize((400, 400))
+    photo = ImageTk.PhotoImage(resized_image)
+    image_label = tk.Label(left_frame, image=photo, borderwidth=0, highlightthickness=0, bg="white")
+    image_label.pack(expand=True)  # Posisi di tengah secara vertikal
+except Exception as e:
+    error_label = tk.Label(left_frame, text="Gambar tidak ditemukan!", fg="red", bg="white")
+    error_label.pack(expand=True)  # Posisi di tengah secara vertikal
 
-password_label = tk.Label(login_frame, text="Password:")
-password_label.pack()
-password_entry = tk.Entry(login_frame, show="*")  
-password_entry.pack()
+# Inisiasi font
+label_font = tkFont.Font(family="Helvetica", size=20, weight="bold")
+entry_font = tkFont.Font(family="Helvetica", size=12)
+button_font = tkFont.Font(family="Helvetica", size=12, weight="bold")
 
-# Tombol Login
-login_button = tk.Button(login_frame, text="Login", command=login)
-login_button.pack(pady=20)
+# Kanan frame
+right_frame = tk.Frame(login_frame, bg="white", width=500)
+right_frame.pack(fill="both", side="right", padx=10, pady=10)
+
+# Sub-frame untuk memusatkan isi
+center_frame = tk.Frame(right_frame, bg="white")
+center_frame.pack(expand=True)  # Isi diatur agar berada di tengah secara vertikal dan horizontal
+
+# Judul
+title_label = tk.Label(center_frame, text="Login as a Admin User", font=label_font, bg="#F9F9F9", fg="#6F42C1")
+title_label.pack(pady=(20, 10))
+
+# Frame untuk input email
+email_frame = tk.Frame(center_frame, bg="#F9F9F9")
+email_frame.pack(pady=(5, 10))
+
+# Email icon
+email_icon = tk.Label(email_frame, text="👤", font=entry_font, bg="#F9F9F9", fg="#6F42C1")
+email_icon.pack(side=tk.LEFT, padx=(0, 10))
+
+# Email entry
+email_entry = tk.Entry(email_frame, font=entry_font, fg="#6F42C1", width=30)
+email_entry.insert(0, "")
+email_entry.pack(side=tk.LEFT)
+
+# Frame untuk input password
+password_frame = tk.Frame(center_frame, bg="#F9F9F9")
+password_frame.pack(pady=(5, 20))
+
+# Password icon
+password_icon = tk.Label(password_frame, text="🔒", font=entry_font, bg="#F9F9F9", fg="#6F42C1")
+password_icon.pack(side=tk.LEFT, padx=(0, 10))
+
+# Password entry
+password_entry = tk.Entry(password_frame, font=entry_font, fg="#6F42C1", width=30, show="*")
+password_entry.insert(0, "")
+password_entry.pack(side=tk.LEFT)
+
+# Login button
+# create_rounded_button(center_frame, x=5, y=5, width=100, height=40, radius=20, text="LOGIN", command=login)
+login_button = tk.Button(center_frame, text="LOGIN", command=login, font=button_font, 
+                         fg="#FFFFFF", bg="#6F42C1", width=20, height=2, relief="flat")
+login_button.pack(pady=(10, 10))
+
 
 # Frame QR Code
 qr_frame = tk.Frame(root , bg="white")
@@ -409,32 +523,92 @@ create_rounded_button(back_canvas, x=5, y=5, width=90, height=40, radius=20, tex
 # Frame Database
 databases_frame = tk.Frame(root)
 
-# Tombol Tambah Siswa di halaman database
-tambah_siswa = tk.Button(databases_frame, text="Tambah Siswa", command=tambahSiswa)
-tambah_siswa.pack(pady=20)
+# Palet warna utama
+primary_color = "#6A1B9A"  # Ungu untuk aksen utama
+background_color = "white"
 
-# Frame Database
-tambahsiswa_frame = tk.Frame(root)
+# Section Tengah (terdiri dari kiri, tengah, dan kanan)
+tengah_frame = tk.Frame(databases_frame, bg="white")
+tengah_frame.place(relx=0.5, rely=0.4, anchor="center", relwidth=0.9, relheight=0.5)
 
-# Mengatur layout komponen pada window tambah siswa
-tk.Button(tambahsiswa_frame, text="Load File Siswa", command=open_file_dialog).grid(row=0, column=0, padx=0, pady=0)
-tk.Radiobutton(tambahsiswa_frame, text="Periode Baru", variable='', value=1).grid(row=0, column=1)
-tk.Radiobutton(tambahsiswa_frame, text="Update Data", variable='', value=2).grid(row=0, column=2)
+# Bagian Kiri di Tengah
+kiri_frame = tk.Frame(tengah_frame, bg=background_color, width=200)
+kiri_frame.pack(fill="y", side="left", padx=10, pady=10)
 
-# Tombol Save di halaman Tamabah Siswa
-btnsave = tk.Button(tambahsiswa_frame, text="Save", command='', state=tk.DISABLED)
-btnsave.grid(row=0, column=4, padx=0, pady=0)
+# Bagian Tengah di Tengah
+tengah_tengah_frame = tk.Frame(tengah_frame, bg=background_color, width=200)
+tengah_tengah_frame.pack(fill="both", side="left", expand=True, padx=10, pady=10)
 
-# Label kolom
-tk.Label(tambahsiswa_frame, text="NISN", width=20, anchor="center").grid(row=1, column=0, padx=0, pady=0)
-tk.Label(tambahsiswa_frame, text="Nama", width=20, anchor="center").grid(row=1, column=1, padx=0, pady=0)
-tk.Label(tambahsiswa_frame, text="No TLP Ortu", width=20, anchor="center").grid(row=1, column=2, padx=0, pady=0)
-tk.Label(tambahsiswa_frame, text="No TLP Walas", width=20, anchor="center").grid(row=1, column=3, padx=0, pady=0)
-tk.Label(tambahsiswa_frame, text="Kelas", width=20, anchor="center").grid(row=1, column=4, padx=0, pady=0)
+# Bagian Kanan di Tengah
+kanan_frame = tk.Frame(tengah_frame, bg=background_color, width=200)
+kanan_frame.pack(fill="y", side="right", padx=10, pady=10)
 
-# Tombol Save di halaman Tamabah Siswa
-btnexit = tk.Button(tambahsiswa_frame, text="Exit", command=tomenudatabase)
-btnexit.grid(row=2, column=5, padx=0, pady=0)
+# Section Bawah
+bawah_frame = tk.Frame(databases_frame, bg=background_color, height=80)
+bawah_frame.pack(fill="x", side="bottom")
+
+# Judul Menu
+title_label = tk.Label(databases_frame, text="Menu Database", bg=background_color, fg="Black", font=("Arial", 24, "bold"))
+title_label.pack(pady=10)
+
+# Load Gambar
+try:
+    image_path = "assets/database.png"  # Pastikan jalur sesuai dengan lokasi file gambar Anda
+    loaded_image = load_image(image_path, 250, 250)  # Mengatur ukuran gambar
+    img_label = tk.Label(kiri_frame, image=loaded_image, bg="white")
+    img_label.image = loaded_image  # Menyimpan referensi agar tidak dihapus oleh garbage collector
+    img_label.pack(pady=20)
+except FileNotFoundError:
+    error_label = tk.Label(kiri_frame, text="Gambar tidak ditemukan!", fg="red", bg="white")
+    error_label.pack(pady=20)
+
+# Tombol Database di halaman Menu
+database = tk.Canvas(kiri_frame, width=110, height=50, bg="white", highlightthickness=0)
+database.place(relx=0.50, rely=0.85, anchor="center") 
+create_rounded_button(database, x=5, y=5, width=100, height=40, radius=20, text="Input Data", command="")
+
+# Load Gambar
+try:
+    image_path = "assets/reicon.png"  # Pastikan jalur sesuai dengan lokasi file gambar Anda
+    loaded_image = load_image(image_path, 250, 250)  # Mengatur ukuran gambar
+    img_label = tk.Label(tengah_tengah_frame, image=loaded_image, bg="white")
+    img_label.image = loaded_image  # Menyimpan referensi agar tidak dihapus oleh garbage collector
+    img_label.pack(pady=20)
+except FileNotFoundError:
+    error_label = tk.Label(tengah_tengah_frame, text="Gambar tidak ditemukan!", fg="red", bg="white")
+    error_label.pack(pady=20)
+
+# Tombol Riport di halaman Menu
+riport = tk.Canvas(tengah_tengah_frame, width=150, height=50, bg="white", highlightthickness=0)
+riport.place(relx=0.50, rely=0.85, anchor="center") 
+create_rounded_button(riport, x=5, y=5, width=140, height=40, radius=20, text="Backup Database", command="")
+
+# Load Gambar
+try:
+    image_path = "assets/abs.png"  # Pastikan jalur sesuai dengan lokasi file gambar Anda
+    loaded_image = load_image(image_path, 250, 250)  # Mengatur ukuran gambar
+    img_label = tk.Label(kanan_frame, image=loaded_image, bg="white")
+    img_label.image = loaded_image  # Menyimpan referensi agar tidak dihapus oleh garbage collector
+    img_label.pack(pady=20)
+except FileNotFoundError:
+    error_label = tk.Label(kanan_frame, text="Gambar tidak ditemukan!", fg="red", bg="white")
+    error_label.pack(pady=20)
+
+# Tombol Absensi Siswa di halaman Menu
+absensi = tk.Canvas(kanan_frame, width=150, height=50, bg="white", highlightthickness=0)
+absensi.place(relx=0.50, rely=0.85, anchor="center") 
+create_rounded_button(absensi, x=5, y=5, width=140, height=40, radius=20, text="Update Presensi", command="")
+
+# Tombol Exit di pojok kanan bawah pada bawah_frame
+exited_canvas = tk.Canvas(bawah_frame, width=100, height=50, bg="white", highlightthickness=0)
+exited_canvas.place(relx=0.85, rely=0.25, anchor="center") 
+create_rounded_button(exited_canvas, x=5, y=5, width=90, height=40, radius=20, text="Exit", command="")
+
+# Frame input data list siswa
+
+# Frame backup database
+
+# Frame update presensi kehadiran siswa
 
 # Frame Riport
 
